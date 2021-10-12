6d83263b6a633a243ff5b9f4dae8577bca71881d
prajakta <prajaktamahajan789@gmail.com>
2021-10-12 13:04:18 +0530

    Added:Create linklist by appending 30 and 70 to 56

--- src/main/java/com/bridgelabz/Link_List.java
+++ src/main/java/com/bridgelabz/Link_List.java
@@ -23,9 +23,22 @@ public class Link_List {
         } else {
             Node temp = head;
             while (temp != null) {
-                System.out.print(temp.data+ " ");
+                if(temp.next != null)
+                    System.out. print(temp.data + " -> ");
+                else System.out.println(temp.data);
                 temp = temp.next;
             }
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            tail = newNode;
         }
     }
 }
